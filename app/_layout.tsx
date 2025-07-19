a2f0501d2f1e72cd2bb5e0892455ef3cae3a229f
Shaharyar <shaharyar4t4@gmail.com>
2025-07-20 00:49:28 +0500

    updated the UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,36 @@
-import 'react-native-reanimated';
-import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function RootLayout() {
   
 
   return (
-     <View></View>  
+     <View>
+         <View style={style.mainbox}>
+            <Text style={style.subtext}>Expo React Native Class 01</Text>
+         </View>
+     </View>  
   );
 }
+
+const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff"
+   },
+   mainbox: {
+      backgroundColor: "#1D2671",
+      width: "100%",
+      height: "20%",
+      borderBottomRightRadius: 20,
+      borderBottomLeftRadius: 20,
+      justifyContent: "center",
+      padding: 20,
+   },
+    subtext: {
+      color: "#fff",
+      fontSize: 20,
+      textAlign: "center",
+      fontWeight: "bold",
+      fontFamily: "Poppins"
+   },
+});
